58c36915516e00e7858ed7eeac75cf6844ac6a99
hagimo90 <hagimo90@gmail.com>
2022-12-19 00:29:46 +0100

    Implementing async feature using Tokio

--- newsapi/src/lib.rs
+++ newsapi/src/lib.rs
@@ -1,112 +1,138 @@
-use url::Url;
+#[cfg(feature = "async")]
+use reqwest::Method;
 use serde::Deserialize;
+use url::Url;
 
 const BASE_URL: &str = "https://newsapi.org/v2";
 
-
-#[derive(thiserror::Error,Debug)]
+#[derive(thiserror::Error, Debug)]
 pub enum NewsApiError {
     #[error("Failed Fetching articles")]
-    RequestFailedError(#[from]ureq::Error),
+    RequestFailedError(#[from] ureq::Error),
     #[error("Failed converting articles to string")]
-    FailedResponseToString(#[from]std::io::Error),
+    FailedResponseToString(#[from] std::io::Error),
     #[error("Parsing article failed")]
-    FailedParsingArticle(#[from]serde_json::Error),
+    FailedParsingArticle(#[from] serde_json::Error),
     #[error("Parsing base url failed")]
-    UrlParsing(#[from]url::ParseError),
+    UrlParsing(#[from] url::ParseError),
     #[error("Request failed:{0}")]
     BadRequest(&'static str),
-
+    #[error("Async connection failed")]
+    #[cfg(feature = "async")]
+    AsyncRequestFailed(#[from] reqwest::Error),
 }
 
-#[derive(Deserialize,Debug)]
+#[derive(Deserialize, Debug)]
 pub struct Article {
-    pub title : String,
-    pub url:String,
+    title: String,
+    url: String,
+}
+impl Article {
+    pub fn title(&self) -> &str {
+        &self.title
+    }
+    pub fn url(&self) -> &str {
+        &self.url
+    }
 }
 
 #[derive(Deserialize)]
 pub struct NewsApiResponse {
-    status:String,
-    pub articles :Vec<Article>,
-    code : Option<String>
-
-
+    status: String,
+    pub articles: Vec<Article>,
+    code: Option<String>,
 }
 impl NewsApiResponse {
     pub fn articles(&self) -> &Vec<Article> {
         &self.articles
     }
-    
 }
 pub enum Endpoint {
     TopHeadlines,
 }
 impl ToString for Endpoint {
-    fn to_string(&self)-> String {
+    fn to_string(&self) -> String {
         match self {
-            Self::TopHeadlines =>"top-headlines".to_string()
+            Self::TopHeadlines => "top-headlines".to_string(),
         }
     }
 }
 pub enum Country {
     Us,
-
 }
 impl ToString for Country {
-    fn to_string(&self)-> String {
+    fn to_string(&self) -> String {
         match self {
-            Self::Us =>"Us".to_string()
+            Self::Us => "Us".to_string(),
         }
     }
 }
 pub struct NewsApi {
-    api_key:String,
-    endpoint:Endpoint,
-    country:Country,
+    api_key: String,
+    endpoint: Endpoint,
+    country: Country,
 }
 
 impl NewsApi {
-    pub fn new(api_key:&str) -> Self {
+    pub fn new(api_key: &str) -> Self {
         NewsApi {
-            api_key:api_key.to_string(),
-            endpoint:Endpoint::TopHeadlines,
-            country:Country::Us,
+            api_key: api_key.to_string(),
+            endpoint: Endpoint::TopHeadlines,
+            country: Country::Us,
         }
-
     }
-    pub fn endpoint(&mut self,endpoint:Endpoint)-> &mut NewsApi {
-        self.endpoint= endpoint;
+    pub fn endpoint(&mut self, endpoint: Endpoint) -> &mut NewsApi {
+        self.endpoint = endpoint;
         self
     }
-    pub fn country(&mut self,country:Country)-> &mut NewsApi {
-        self.country= country;
+    pub fn country(&mut self, country: Country) -> &mut NewsApi {
+        self.country = country;
         self
     }
-    fn prepare_url(&self)-> Result<String, NewsApiError> {
+    fn prepare_url(&self) -> Result<String, NewsApiError> {
         let mut url = Url::parse(BASE_URL)?;
-        url.path_segments_mut().unwrap().push(&self.endpoint.to_string());
-        let country = format!("country={}",self.country.to_string());
+        url.path_segments_mut()
+            .unwrap()
+            .push(&self.endpoint.to_string());
+        let country = format!("country={}", self.country.to_string());
         url.set_query(Some(&country));
 
         Ok(url.to_string())
     }
 
-    pub fn fetch(&self) -> Result<NewsApiResponse,NewsApiError> {
+    pub fn fetch(&self) -> Result<NewsApiResponse, NewsApiError> {
         let url = self.prepare_url()?;
-        let req = ureq::get(&url).set("Authorization",&self.api_key);
-        let response:NewsApiResponse = req.call()?.into_json()?;
+        let req = ureq::get(&url).set("Authorization", &self.api_key);
+        let response: NewsApiResponse = req.call()?.into_json()?;
         match response.status.as_str() {
             "ok" => return Ok(response),
-            _ => return Err(map_response_err(response.code))
+            _ => return Err(map_response_err(response.code)),
         }
-
     }
+    #[cfg(feature = "async")]
+    pub async fn fetch_async(&self) -> Result<NewsApiResponse, NewsApiError> {
+        let url = self.prepare_url()?;
+        let client = reqwest::Client::new();
+        let request = client
+            .request(Method::GET, url)
+            .header("Authorization", &self.api_key)
+            .build()
+            .map_err(|e| NewsApiError::AsyncRequestFailed(e))?;
 
-
+        let response: NewsApiResponse = client
+            .execute(request)
+            .await?
+            .json()
+            .await
+            .map_err(|e| NewsApiError::AsyncRequestFailed(e))?;
+        match response.status.as_str() {
+            "ok" => return Ok(response),
+            _ => return Err(map_response_err(response.code)),
+        }
+    }
 }
 
-fn map_response_err(code:Option<String>) -> NewsApiError {
+fn map_response_err(code: Option<String>) -> NewsApiError {
     if let Some(code) = code {
         match code.as_str() {
             "apiKeyDisable" => NewsApiError::BadRequest("Your API key has been disabled."),
--- src/main.rs
+++ src/main.rs
@@ -9,27 +9,23 @@ fn render_articles(articles : &Vec<Article>){
     let theme = theme::default();
     theme.print_text("~#Top Headlines \n\n");
         for a in articles {
-            theme.print_text(&format!("> `{}`\n",a.title));
-            theme.print_text(&format!("> `{}`\n",a.url));
+            theme.print_text(&format!("> `{}`\n",a.title()));
+            theme.print_text(&format!("> `{}`\n",a.url()));
             theme.print_text("------");
         }
 }
 
-
-fn main() -> Result<(),Box<dyn Error>> {
+#[tokio::main]
+async fn main() -> Result<(),Box<dyn Error>> { 
     dotenv()?;
     let api_key = std::env::var("API_KEY")?;
 
     let mut newsapi = NewsApi::new(&api_key);
     newsapi.endpoint(Endpoint::TopHeadlines).country(Country::Us);
-    let news_api_response = newsapi.fetch()?;
+    let news_api_response = newsapi.fetch_async().await?;
     render_articles(&news_api_response.articles());
  
 
-//
-   // let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=";
-    //let url = format!("{}{}",url,api_key);
-    //let articles= get_articles(&url)?;
-    //render_articles(&articles);
+
     Ok(())
 }
