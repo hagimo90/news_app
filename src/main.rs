5aed692c4673acbcf58f43bdb87f90e3a6a8c2b7
hagimo90 <hagimo90@gmail.com>
2022-12-18 20:16:39 +0100

    APi connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::error::Error;
+fn get_articles(url:&str)-> Result<Articles,Box<dyn Error>>{
+    let reponse = ureq::get(url).call()?.into_string();
+    dbg!(reponse);
+    todo!()
+}
+struct Articles {
+    articles : Vec<Article>
+}
+struct Article {
+
+}
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=e8c250e803ed41a89156fe9f351b8acc";
+    let aricles= get_articles(url);
 }
