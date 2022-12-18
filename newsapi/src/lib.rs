275c72e4529aaee306d6ba0e7a80cd5ca17b94d8
hagimo90 <hagimo90@gmail.com>
2022-12-18 23:35:09 +0100

    API library design

--- newsapi/src/lib.rs
+++ newsapi/src/lib.rs
@@ -1,28 +1,128 @@
-
+use url::Url;
 use serde::Deserialize;
+
+const BASE_URL: &str = "https://newsapi.org/v2";
+
+
 #[derive(thiserror::Error,Debug)]
 pub enum NewsApiError {
     #[error("Failed Fetching articles")]
-    RequestFailedError(ureq::Error),
+    RequestFailedError(#[from]ureq::Error),
     #[error("Failed converting articles to string")]
-    FailedResponseToString(std::io::Error),
+    FailedResponseToString(#[from]std::io::Error),
     #[error("Parsing article failed")]
-    FailedParsingArticle(serde_json::Error)
+    FailedParsingArticle(#[from]serde_json::Error),
+    #[error("Parsing base url failed")]
+    UrlParsing(#[from]url::ParseError),
+    #[error("Request failed:{0}")]
+    BadRequest(&'static str),
 
 }
-#[derive(Deserialize,Debug)]
-pub struct Articles {
-    pub articles : Vec<Article>
-}
+
 #[derive(Deserialize,Debug)]
 pub struct Article {
     pub title : String,
     pub url:String,
 }
-pub fn get_articles(url:&str)-> Result<Articles,NewsApiError>{
-    let response = ureq::get(url).call().map_err(|err| NewsApiError::RequestFailedError(err))?.into_string().map_err(|err| NewsApiError::FailedResponseToString(err))?;
-    let articles:Articles = serde_json::from_str(&response).map_err(|err| NewsApiError::FailedParsingArticle(err))?;
 
-    Ok(articles)
+#[derive(Deserialize)]
+pub struct NewsApiResponse {
+    status:String,
+    pub articles :Vec<Article>,
+    code : Option<String>
+
+
+}
+impl NewsApiResponse {
+    pub fn articles(&self) -> &Vec<Article> {
+        &self.articles
+    }
     
+}
+pub enum Endpoint {
+    TopHeadlines,
+}
+impl ToString for Endpoint {
+    fn to_string(&self)-> String {
+        match self {
+            Self::TopHeadlines =>"top-headlines".to_string()
+        }
+    }
+}
+pub enum Country {
+    Us,
+
+}
+impl ToString for Country {
+    fn to_string(&self)-> String {
+        match self {
+            Self::Us =>"Us".to_string()
+        }
+    }
+}
+pub struct NewsApi {
+    api_key:String,
+    endpoint:Endpoint,
+    country:Country,
+}
+
+impl NewsApi {
+    pub fn new(api_key:&str) -> Self {
+        NewsApi {
+            api_key:api_key.to_string(),
+            endpoint:Endpoint::TopHeadlines,
+            country:Country::Us,
+        }
+
+    }
+    pub fn endpoint(&mut self,endpoint:Endpoint)-> &mut NewsApi {
+        self.endpoint= endpoint;
+        self
+    }
+    pub fn country(&mut self,country:Country)-> &mut NewsApi {
+        self.country= country;
+        self
+    }
+    fn prepare_url(&self)-> Result<String, NewsApiError> {
+        let mut url = Url::parse(BASE_URL)?;
+        url.path_segments_mut().unwrap().push(&self.endpoint.to_string());
+        let country = format!("country={}",self.country.to_string());
+        url.set_query(Some(&country));
+
+        Ok(url.to_string())
+    }
+
+    pub fn fetch(&self) -> Result<NewsApiResponse,NewsApiError> {
+        let url = self.prepare_url()?;
+        let req = ureq::get(&url).set("Authorization",&self.api_key);
+        let response:NewsApiResponse = req.call()?.into_json()?;
+        match response.status.as_str() {
+            "ok" => return Ok(response),
+            _ => return Err(map_response_err(response.code))
+        }
+
+    }
+
+
+}
+
+fn map_response_err(code:Option<String>) -> NewsApiError {
+    if let Some(code) = code {
+        match code.as_str() {
+            "apiKeyDisable" => NewsApiError::BadRequest("Your API key has been disabled."),
+            "apiKeyExhausted" => NewsApiError::BadRequest("Your API key has no more requests available."),
+            "apiKeyInvalid" => NewsApiError::BadRequest("Your API key hasn't been entered correctly. Double check it and try again."),
+            "apiKeyMissing" => NewsApiError::BadRequest("Your API key is missing from the request."),
+            "parameterInvalid" => NewsApiError::BadRequest("You've included a parameter in your request which is currently not supported. "),
+            "parametersMissing" => NewsApiError::BadRequest("Required parameters are missing from the request and it cannot be completed. "),
+            "rateLimited" => NewsApiError::BadRequest("You have been rate limited. Back off for a while before trying the request again."),
+            "sourcesTooMany" => NewsApiError::BadRequest("You have requested too many sources in a single request. Try splitting the request into 2 smaller requests."),
+            "sourceDoesNotExist" => NewsApiError::BadRequest("You have requested a source which does not exist."),
+            _ => NewsApiError::BadRequest("This shouldn't happen, and if it does then it's our fault, not yours. Try the request again shortly."),
+
+
+        }
+    } else {
+        NewsApiError::BadRequest("This shouldn't happen, and if it does then it's our fault, not yours. Try the request again shortly.")
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,13 @@ mod theme;
 use std::error::Error;
 use dotenv::dotenv;
 
-use newsapi::{Articles, get_articles};
+use newsapi::{NewsApi,Endpoint,Country, Article};
 
 
-
-fn render_articles(articles : &Articles){
+fn render_articles(articles : &Vec<Article>){
     let theme = theme::default();
     theme.print_text("~#Top Headlines \n\n");
-        for a in &articles.articles {
+        for a in articles {
             theme.print_text(&format!("> `{}`\n",a.title));
             theme.print_text(&format!("> `{}`\n",a.url));
             theme.print_text("------");
@@ -20,9 +19,17 @@ fn render_articles(articles : &Articles){
 fn main() -> Result<(),Box<dyn Error>> {
     dotenv()?;
     let api_key = std::env::var("API_KEY")?;
-    let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=";
-    let url = format!("{}{}",url,api_key);
-    let articles= get_articles(&url)?;
-    render_articles(&articles);
+
+    let mut newsapi = NewsApi::new(&api_key);
+    newsapi.endpoint(Endpoint::TopHeadlines).country(Country::Us);
+    let news_api_response = newsapi.fetch()?;
+    render_articles(&news_api_response.articles());
+ 
+
+//
+   // let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=";
+    //let url = format!("{}{}",url,api_key);
+    //let articles= get_articles(&url)?;
+    //render_articles(&articles);
     Ok(())
 }
